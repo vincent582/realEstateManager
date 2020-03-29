8ad803ba157d22a41505007a075042eaad99b822
Vincent582 <pasdeloupvincent@live.fr>
2020-03-29 14:57:49 +0900

    -add convert euro to dollar function & change date format -add unitTest for Utils class

--- app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/Utils.java
@@ -7,10 +7,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * Created by Philippe on 21/02/2018.
- */
-
 public class Utils {
 
     /**
@@ -24,12 +20,22 @@ public class Utils {
     }
 
     /**
+     * Conversion d'un prix d'un bien immobilier (Euros vers Dollars)
+     * @param euros
+     * @return
+     */
+    public static int convertEuroToDollar(int euros){
+        return (int) Math.round(euros / 0.812);
+    }
+
+
+    /**
      * Conversion de la date d'aujourd'hui en un format plus approprié
      * NOTE : NE PAS SUPPRIMER, A MONTRER DURANT LA SOUTENANCE
      * @return
      */
     public static String getTodayDate(){
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         return dateFormat.format(new Date());
     }
 
---
+++ app/src/test/java/com/openclassrooms/realestatemanager/UtilsTest.java
@@ -0,0 +1,43 @@
+package com.openclassrooms.realestatemanager;
+
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+
+import static org.junit.Assert.*;
+
+public class UtilsTest {
+
+    int mDollars = 100;
+    int mEuros = 81;
+
+    @Test
+    public void convertDollarToEuro() {
+        int euros = Utils.convertDollarToEuro(mDollars);
+        assertEquals(euros,81);
+    }
+
+    @Test
+    public void convertEuroToDollar() {
+        int dollars = Utils.convertEuroToDollar(mEuros);
+        assertEquals(dollars,100);
+    }
+
+    @Test
+    public void getTodayDate() {
+        String date = Utils.getTodayDate();
+        assertTrue(isValidDate(date));
+    }
+
+    public boolean isValidDate(String date) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+            sdf.setLenient(false);
+            sdf.parse(date);
+        }
+        catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
