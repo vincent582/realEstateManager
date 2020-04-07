c63d2ccfc7c80e6f0a1690831346e70a3319af25
Vincent582 <pasdeloupvincent@live.fr>
2020-04-07 16:38:04 +0900

    -add database class

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/Database/RealEstateManagerDataBase.java
@@ -0,0 +1,37 @@
+package com.openclassrooms.realestatemanager.Database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.openclassrooms.realestatemanager.Database.DAO.AddressDAO;
+import com.openclassrooms.realestatemanager.Database.DAO.PictureDAO;
+import com.openclassrooms.realestatemanager.Database.DAO.PropertyDAO;
+import com.openclassrooms.realestatemanager.Model.Address;
+import com.openclassrooms.realestatemanager.Model.Picture;
+import com.openclassrooms.realestatemanager.Model.Property;
+
+@Database(entities = {Property.class, Address.class, Picture.class},version = 1,exportSchema = false)
+public abstract class RealEstateManagerDataBase extends RoomDatabase {
+
+    public static volatile RealEstateManagerDataBase INSTANCE;
+
+    public abstract PropertyDAO mPropertyDAO();
+    public abstract AddressDAO mAddressDAO();
+    public abstract PictureDAO mPictureDAO();
+
+    public static RealEstateManagerDataBase getInstance(Context context){
+        if (INSTANCE == null){
+            synchronized (RealEstateManagerDataBase.class){
+                if (INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            RealEstateManagerDataBase.class,"RealEstateManagerDatabase.db")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
