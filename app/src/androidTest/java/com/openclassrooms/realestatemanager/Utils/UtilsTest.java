4450524ff77aa33f4a857a5d66cb5e6f8bd5239a
Vincent582 <pasdeloupvincent@live.fr>
2020-04-26 23:09:10 +0900

    add test for internet connection

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/Utils/UtilsTest.java
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/Utils/UtilsTest.java
@@ -1,25 +1,38 @@
 package com.openclassrooms.realestatemanager.Utils;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.*;
 
+@RunWith(AndroidJUnit4.class)
 public class UtilsTest {
 
     private Context appContext;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         appContext = InstrumentationRegistry.getTargetContext();
     }
 
     @Test
     public void isInternetAvailable() {
-
+        ConnectivityManager connectivityManager = (ConnectivityManager) appContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = null;
+        if(connectivityManager != null){
+            activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        }
+        Boolean isActiveNetwork = activeNetworkInfo.isConnected();
+        assertEquals(isActiveNetwork,Utils.isInternetAvailable(appContext));
     }
+
 }
